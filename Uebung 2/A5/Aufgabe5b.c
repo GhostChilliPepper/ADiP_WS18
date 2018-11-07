7b384c61b0c924cb5842f93d8d299150d8e11d7f
lgoette <lasse@wildtfeuer.de>
2018-11-07 11:56:16 +0100

    A5b

--- Uebung 2/A5/Aufgabe5b.c
+++ Uebung 2/A5/Aufgabe5b.c
@@ -10,10 +10,24 @@ int exponent(double a, int n) {
     double ergebniss = a;
     if (n==0) {
         ergebniss = 1;
+    } else if (n > 1) {
+        for (int i = 1; i < n; i++) {
+            ergebniss = ergebniss * a;
+            count++;
+        }
+    }
+    return ergebniss;
+}
+
+int exponential(double a, int n) {
+    double ergebniss = a;
+    if (n==0) {
+        ergebniss = 1;
     } else if (n%2==0) {
-
+        ergebniss = exponent(a, n/2) * exponent(a, n/2);
+        count++;
     } else if (n%2!=0) {
-
+        ergebniss = a * exponent(a, (n-1)/2) * exponent(a, (n-1)/2)
     }
     return ergebniss;
 }
@@ -22,9 +36,9 @@ int main(void) {
     double z;
     int e;
     printf("Geben sie eine Zahl an: ");
-    scanf("%i", &z);
+    scanf("%le", &z);
     printf("Geben sie einen Exponenten an: ");
     scanf("%i", &e);
-    printf("Ergebniss: %i\n", exponent(z, e));
+    printf("Ergebniss: %i\n", exponential(z, e));
     printf("Schritte: %i\n", count);
 }
