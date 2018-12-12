c0946e8453edba03697c8c3fd82d3330639f1f1a
alex <s6alhaer@uni-bonn.de>
2018-12-13 00:11:49 +0100

    Mit ein wenig Unterstützung meiner Bibliothek endlich geschafft.

--- Uebung 6/A2/students.c
+++ Uebung 6/A2/students.c
@@ -3,54 +3,52 @@
  * Program: students.c
  * Autohor: lasse
  * Date: 12/12/18
- * Purpose: 
+ * Purpose:
  * * * * * * * * * * * * * * * * * * * * * */
 
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 
 typedef struct {
     char nachname[30];
     float note;
 } student;
-
-student * newStudent(char pNachname[], float pNote);
-
-void clearBuffer() {
-    char c;
-    while ((c = getchar()) != '\n' && c != EOF) {}
-}
 int main() {
-    char name[30];
-    float note;
     int Anzahl;
+    float sum;
+    float Minimum=5.0;
+    student *Array;
+
     printf("Gib die Anzahl der Studenten an!\n");
     scanf("%d",&Anzahl);
-    clearBuffer();
+
+    Array = (student*) malloc(Anzahl*(sizeof(student)));
+    if(Array == NULL){
+        printf("Der Speicher ist leer!\n");
+        exit(1);
+    }
+
     for (int i = 0; i < Anzahl; ++i) {
         printf("Gib einen Nachnamen ein!\n");
-        scanf("%c",name);
-        clearBuffer();
+        scanf("%s",Array[i].nachname);
         printf("Gib eine Note von 1.0 bis 5.0 ein!\n");
-        scanf("%f",&note);
-        while(note<1.0||note>5.0){
+        scanf("%f",&Array[i].note);
+        while(Array[i].note<1.0||Array[i].note>5.0){
             printf("Gib eine Note von 1.0 bis 5.0 ein!\n");
-            clearBuffer();
-            scanf("%f",&note);
+            scanf("%f",&Array[i].note);
         }
-        clearBuffer();
-        newStudent(name,note);
+        if(Array[i].note<Minimum){
+            Minimum=Array[i].note;
+        }
+        sum+= Array[i].note;
     }
-
+    printf("Dies sind die besten Studenten!\n");
+    for (int j = 0; j < Anzahl; ++j) {
+        if (Array[j].note == Minimum){
+            printf("%s\n", Array[j].nachname);
+        }
+    }
+    sum=sum/Anzahl;
+    printf("Die Durchschnittsnote aller Studenten ist: %f !\n", sum);
     return 0;
-}
-
-student * newStudent(char pNachname[30], float pNote) {
-    student *output = malloc(sizeof(student));
-    for (int i = 0; i < 30; ++i) {
-        output->nachname[i]=pNachname[i];
-    }
-    output->note = pNote;
-    return output;
-
 }
