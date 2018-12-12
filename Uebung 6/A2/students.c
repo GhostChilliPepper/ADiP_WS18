b9ccc7920b415feb3bff7e46880f26efe77fa8da
alex <s6alhaer@uni-bonn.de>
2018-12-12 17:30:56 +0100

    Da fehlt noch das ausgeben der Studenten mit den besten Noten und die Ausgabe des Durchschnitts.

--- Uebung 6/A2/students.c
+++ Uebung 6/A2/students.c
@@ -7,28 +7,50 @@
  * * * * * * * * * * * * * * * * * * * * * */
 
 #include <stdio.h>
-#include <string.h>
+#include <malloc.h>
 
 typedef struct {
     char nachname[30];
     float note;
 } student;
 
-student newStudent(char pNachname[], float pNote);
+student * newStudent(char pNachname[], float pNote);
 
+void clearBuffer() {
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
 int main() {
-    char name[30] = "bla";
-    float note = 5.0;
-    student s = newStudent(name, note);
-    printf("%s,%f", s.nachname, s.note);
+    char name[30];
+    float note;
+    int Anzahl;
+    printf("Gib die Anzahl der Studenten an!\n");
+    scanf("%d",&Anzahl);
+    clearBuffer();
+    for (int i = 0; i < Anzahl; ++i) {
+        printf("Gib einen Nachnamen ein!\n");
+        scanf("%c",name);
+        clearBuffer();
+        printf("Gib eine Note von 1.0 bis 5.0 ein!\n");
+        scanf("%f",&note);
+        while(note<1.0||note>5.0){
+            printf("Gib eine Note von 1.0 bis 5.0 ein!\n");
+            clearBuffer();
+            scanf("%f",&note);
+        }
+        clearBuffer();
+        newStudent(name,note);
+    }
+
     return 0;
 }
 
-student newStudent(char pNachname[30], float pNote) {
-    student output;
-    for (int i = 0; i < 30; i++) {
-        output.nachname[i] = pNachname[i];
+student * newStudent(char pNachname[30], float pNote) {
+    student *output = malloc(sizeof(student));
+    for (int i = 0; i < 30; ++i) {
+        output->nachname[i]=pNachname[i];
     }
-    output.note = pNote;
+    output->note = pNote;
     return output;
+
 }
