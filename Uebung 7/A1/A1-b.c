ed3ece2bea7d95a42dd8a112210001055b868ee1
lgoette <lasse@wildtfeuer.de>
2018-12-20 14:56:10 +0100

    A1-b

--- Uebung 7/A1/A1-b.c
+++ Uebung 7/A1/A1-b.c
@@ -15,37 +15,70 @@ typedef struct DoubleNode{
 } DoubleNode;
 
 void printList(DoubleNode *head);
-DoubleNode insertFirst(DoubleNode* head_ref, double new_data);
-
+void insertFirst(DoubleNode** head_ref, double new_data);
+void insertLast(DoubleNode** head_ref, double new_data);
+double get(DoubleNode *head,int index);
 
 int main(){
-    for(int i = 1; int i <= 10; i++) {
-        DoubleNode output = NULL;
-        DoubleNode insertFirst(*output, i);
-        printList(*output);
+    DoubleNode* output = NULL;
+    for(int i = 1; i <= 10; i++) {
+        insertFirst(&output, i);
     }
+    insertLast(&output, 10.12);
+    printList(output);
+    printf("\n");
+    printf("Index: 5 - %lf\n", get(output, 5));
     return 0;
 }
 
-DoubleNode insertFirst(DoubleNode* head_ref, double new_data) {
-    DoubleNode new_node = (DoubleNode) malloc(sizeof(struct DoubleNode));
-    new_node.data  = new_data;
-    new_node.next = *head_ref;
-    return new_node;
+double get(DoubleNode *head, int index) {
+    int counter = 1;
+    while(head != NULL && counter < index) {
+        counter++;
+        head = head->next;
+    }
+    if(counter == index) {
+        return head->data;
+    } else {
+        return -1;
+    }
+}
+
+void insertFirst(DoubleNode** head_ref, double new_data) {
+    DoubleNode* new_node = (DoubleNode*) malloc(sizeof(struct DoubleNode));
+    new_node->data  = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref)    = new_node;
+}
+
+void insertLast(DoubleNode** head_ref, double new_data) {
+    DoubleNode* new_node = (DoubleNode*) malloc(sizeof(struct DoubleNode));
+    DoubleNode* vorher;
+    DoubleNode* ende = NULL;
+    DoubleNode *temp = *head_ref;
+
+    new_node->data  = new_data;
+    while(temp != NULL) {
+        vorher = temp;
+        temp = temp->next;
+    }
+    vorher->next = new_node;
+    new_node->next = ende;
 }
 
 void printList(DoubleNode *head) {
     DoubleNode *temp = head;
-    printf ("( ");
 
     while(temp != NULL)
     {
-        printf("%lf  ", temp->data);
+        printf ("[");
+        printf("%lf", temp->data);
+        printf("]");
+        if(temp->next != NULL){
+            printf(" -> ");
+        }
         temp = temp->next;
     }
-    printf(")");
-
-    printf("\n");
 }
 /*
 void reverseList(intNode** head_ref)
